81bd21f2ae94d3e9f7e2a965875acca0651c324a
Matthew Olsen <mbolsen@gmail.com>
2022-01-16 14:40:02 -0700

    GET request added to the front end via axios also returning result from the API

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 // ---IMPORT COMPONENTS---
 
@@ -14,8 +15,16 @@ export const App = function () {
     setText(event.target.value);
   };
 
-  const handleSubmit = () => {
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send request to the API.
+    // Since we are using the localhost there is no need to put the full URL,
+    // If this client app was not served by the same server we would need to add the full URL of the API
+    axios.get('/openpulls', { text })
+      .then((res) => {
+        // temporary console.log for testing purposes
+        console.log(res);
+      });
   };
 
   // ---USE EFFECT---
@@ -26,7 +35,12 @@ export const App = function () {
       <AppContext.Provider value={{ }}>
         <div>
           <h1>GitHub Repository Open Pull Requests</h1>
-          <form>
+          <form
+            onSubmit={(event) => {
+              // event.preventDefault();
+              handleSubmit(event);
+            }}
+          >
             <label>
               GitHub Repository URL:
               <input type="text" onChange={handleTextInput} />
