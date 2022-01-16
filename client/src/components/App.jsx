bd1a2409a6a5e7c5a6646ec68a8352b3506d1b71
Matthew Olsen <mbolsen@gmail.com>
2022-01-16 15:53:26 -0700

    after receiving the github repo url we break apart that url so we can use it later

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -18,7 +18,7 @@ export const App = function () {
     // Send request to the API.
     // Since we are using the localhost there is no need to put the full URL,
     // If this client app was not served by the same server we would need to add the full URL of the API
-    axios.get('/openpulls', { text })
+    axios.get('/open-pulls', { params: { url: text } })
       .then((res) => {
         // set the response state equal to the response from the get request
         setResponse(res);
--- server/controller/openPulls.js
+++ server/controller/openPulls.js
@@ -1,7 +1,20 @@
+const axios = require('axios');
+
 module.exports = {
-  get: function (req, res) {
-    //NOTE: this is the initial get request that will just return the req, this will change in a future commit.
-    console.log(req);
+  get(req, res) {
+    // PSUEDOCODE:
+    // GOAL: need to return the number of commits for each open pull request
+    // repoURL is the github repository url that the client is requesting
+    const repoURL = req.query.url;
+    // usernameAndRepo is the section of repoURL that only contains the 'username/repo'
+    const usernameAndRepo = repoURL.slice(repoURL.indexOf('.com/') + 5);
+    console.log(usernameAndRepo);
+    // pullRequestURL will be the URL we need to use to fetch information about pull requests
+    const pullRequestURL = `https://api.github.com/repos/${usernameAndRepo}/pulls`;
+    // then we need to request from the github API the information about that repo's pulls
+    // after this we need to change the information to only display open pull requests
+    // after this we need to request from the github API the number of commits per each open request
+    axios.get('https://api.github.com/repos/mbolsen/javascript-app-templates/pulls');
     res.status(400).send('returning request');
-  }
-}
+  },
+};
