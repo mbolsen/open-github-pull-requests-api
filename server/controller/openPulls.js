f9170cff10a5dec7be28d14007cafe4db14d9595
Matthew Olsen <mbolsen@gmail.com>
2022-01-16 20:21:07 -0700

    clients are able to request a github url and it return the number of commits/open url

--- server/controller/openPulls.js
+++ server/controller/openPulls.js
@@ -1,38 +1,51 @@
 const axios = require('axios');
 
-const findNumberOfCommits = (commitURL) => 1;
-
 module.exports = {
   get(req, res) {
+    // GOAL: return the number of commits for each open pull request
+    // return the result as [ {pull_request_title: 'xxx', number_of_commits: 8}, {}, {}, ....]
     const openPRCommits = [];
-    // GOAL: need to return the number of commits for each open pull request
-    // return the reulst as [ {pull request title: 'xxx', number of commits: 8}, {}, {}]
+    const promises = [];
+    const config = {
+      headers: {
+        Authorization: process.env.GITHUB_TOKEN,
+      },
+    };
+
     // repoURL is the github repository url that the client is requesting
     const repoURL = req.query.url;
     // usernameAndRepo is the section of repoURL that only contains the 'username/repo'
     const usernameAndRepo = repoURL.slice(repoURL.indexOf('.com/') + 5);
-    console.log(usernameAndRepo);
     // pullRequestURL will be the URL we need to use to fetch information about pull requests
     const pullRequestURL = `https://api.github.com/repos/${usernameAndRepo}/pulls`;
     // then we need to request from the github API the information about that repo's pulls
-    axios.get(pullRequestURL)
+    axios.get(pullRequestURL, config)
       .then(async (response) => {
-        // console.log('here', res.data);
         for (let i = 0; i < response.data.length; i += 1) {
-          console.log(i, '\n\n', response.data[i].title, '\n', response.data[i].commits_url);
-          const numCommits = await findNumberOfCommits(response.data[i].commits_url);
-          openPRCommits.push({
-            pull_request_title: response.data[i].title,
-            number_of_commits: numCommits,
-          });
+          // push each GET request into an array
+          promises.push(
+            axios.get(response.data[i].commits_url, config)
+              .then((result) => {
+                // after the axios request push the result in openPRCommits
+                openPRCommits.push({
+                  pull_request_title: response.data[i].title,
+                  number_of_commits: result.data.length,
+                });
+              }),
+          );
         }
-        return openPRCommits;
+        // once all of the promises are finished then send the results
+        Promise.all(promises)
+          .then(() => {
+            res.send(openPRCommits);
+          });
       })
-      .then((response) => {
-        console.log(response);
-        res.send(response);
+    // if there is an error, then send the error to the client
+    // github does have a rate limit on requests,
+    // if requesting too much then the client needs a GITHUB TOKEN or to wait until it clears
+      .catch((err) => {
+        // we send a 200, because we want the message to display for the user
+        res.status(200).send(`Error: ${err.response.statusText}`);
       });
-    // after this we need to change the information to only display open pull requests
-    // after this we need to request from the github API the number of commits per each open request
   },
 };
